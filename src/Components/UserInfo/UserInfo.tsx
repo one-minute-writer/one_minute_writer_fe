1323ed75dd9ceba992a50fa7ec433ef4ff9609f4
Gabby Recny <gjtrecny@gmail.com>
2022-03-26 16:09:28 -0600

    Fix changes by adding props and an interface

--- src/Components/UserInfo/UserInfo.tsx
+++ src/Components/UserInfo/UserInfo.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import './UserInfo.scss';
 
+interface Props {
+  words_per_minute: number;
+  total_words: number;
+  userName: string;
+}
 
-const UserInfo: React.FC = () => {
+const UserInfo: React.FC<Props> = (props: Props) => {
+  const displaySadPath = () => {
+    if ('words_per_minute === 0') {
+      return `Looks like we don't have any stats for you yet -- start writing today!`
+    }
+
+  }
   return (
     <section className='user-info'>
         <h3>Welcome, *UserName*</h3>
@@ -12,7 +23,7 @@ const UserInfo: React.FC = () => {
               <p> 1932 </p>
             </article>
             <article className='user-stats'>
-              <p>Words per day: </p>
+              <p>Total Words: </p>
               <p> 23243 </p>
             </article>
       </section>
