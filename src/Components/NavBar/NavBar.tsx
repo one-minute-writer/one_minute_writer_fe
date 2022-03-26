b103a32cd25b59fcd30c8c6aa5d52fe55fcd81bd
Lynn Nguyen <AmandaN22@outlook.com>
2022-03-26 17:03:40 -0600

    Add initial styling to navbar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 import './NavBar.scss';
+import writingLogo from './WritingLogo.png';
+import { NavLink } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
   return (
-    <nav>
+    <header>
+      <img className='logo' src={writingLogo} alt='One Minute Writer'/>
       <h1>One Minute Writer</h1>
-    </nav>
+      <nav>
+        <ul className='menu'>
+            <li>
+              <NavLink to='/' className='selected' id="dashboard-nav-button">Dashboard</NavLink>
+            </li>
+            <li>
+              <NavLink to='/start-writing' className='selected' id="start-writing-nav-button">Start Writing</NavLink>
+            </li>
+          </ul>
+      </nav>
+    </header>
   )
 }
 
