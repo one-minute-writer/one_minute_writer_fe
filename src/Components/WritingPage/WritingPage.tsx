b9bc291887d4362df16b1fdd6f79b8f872f6d47a
Phil Lewis <phillewis1985@gmail.com>
2022-03-26 19:32:19 -0600

    Add start and stop functions in Writing Page

--- src/Components/WritingPage/WritingPage.tsx
+++ src/Components/WritingPage/WritingPage.tsx
@@ -3,14 +3,29 @@ import './Dashboard.scss';
 import NavBar from '../NavBar/NavBar';
 import Inspirations from '../Inspirations/Inspirations';
 import TextInput from '../TextInput/TextInput';
+import Timer from '../Timer/Timer'
 
 const WritingPage: React.FC = () => {
+  const [ writingInProgress, toggleWritingInProgress ] = useState(false)
+  let total_seconds = 0
+
+  const stopTimer = () => {
+    toggleWritingInProgress(!writingInProgress)
+  }
+
+  const startTimer = () => {
+    toggleWritingInProgress(!writingInProgress)
+  }
 
   return (
     <>
       <NavBar />
       <section className='writing-inspiration'>
         <Inspirations />
+        <Timer
+          total_seconds={total_seconds}
+          stopTimer={stopTimer}
+        />
         <TextInput />
       </section>
     </>
