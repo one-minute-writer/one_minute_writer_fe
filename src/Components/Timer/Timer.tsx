505081812602c5acf5446303a48bc0892bd4d000
Phil Lewis <phillewis1985@gmail.com>
2022-03-26 19:12:30 -0600

    Type stopTimer function and fix bug in formatTime function

--- src/Components/Timer/Timer.tsx
+++ src/Components/Timer/Timer.tsx
@@ -2,16 +2,17 @@ import React, { useState, useEffect } from 'react'
 import './Timer.scss'
 
 interface Props {
-  total_seconds: number
+  total_seconds: number,
+  stopTimer: () => {}
 }
 
-const Timer: React.FC<Props> = ({ total_seconds }) => {
+const Timer: React.FC<Props> = ({ total_seconds, stopTimer }) => {
   const [ elapsedTime, setElapsedTime ] = useState(0)
   const [ inProgress, toggleInProgress ] = useState(false)
   let colorClass: string = 'green'
 
   useEffect(() => {
-    inProgress ? setTimeout(() => {setElapsedTime(elapsedTime + 1)}, 1000) : null
+    inProgress && setTimeout(() => {setElapsedTime(elapsedTime + 1)}, 1000)
   }, [inProgress])
 
   useEffect(() => {
@@ -29,15 +30,15 @@ const Timer: React.FC<Props> = ({ total_seconds }) => {
   }
 
   const formatTime = () => {
+    const minutes = Math.floor(elapsedTime / 60)
     const seconds = elapsedTime % 60
-    const minutes = elapsedTime / 60 - seconds
     return `${minutes}:${seconds}`
   }
 
   return (
     <section className={colorClass}>
       <button onClick={toggleTimer}>{formatTime()}</button>
-      {/* <button onClick={stopTimer}>STOP</button> */}
+      <button onClick={stopTimer}>STOP</button>
     </section>
   )
 }
