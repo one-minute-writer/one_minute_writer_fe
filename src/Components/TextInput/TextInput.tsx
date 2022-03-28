9723434791e9434d77387a961a3917f638b39856
Gabby Recny <gjtrecny@gmail.com>
2022-03-28 08:20:57 -0600

    Debug text input TS errors by assigning entry to string

--- src/Components/TextInput/TextInput.tsx
+++ src/Components/TextInput/TextInput.tsx
@@ -2,23 +2,24 @@ import React, { useState } from 'react';
 import './TextInput.scss';
 
 const TextInput: React.FC = () => {
-  const [ entry, setEntry ] = useState<string | {}>()
+  const [ entry, setEntry ] = useState<string>()
 
   //Place for posting the writing entry once completed
   //Did not inpout any actual functionality until we know our data
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault()
     console.log(entry)
   }
 
+  
   return (
     <>
       <form>
         <textarea
           aria-label='textarea'
           placeholder='Begin writing here...'
-          //Lines 20 and 21 have TS errors that I did not
-          type='text'
+          rows={20}
+          cols={30}
           value={entry}
           onChange={(event) => setEntry(event.target.value)}
         >
