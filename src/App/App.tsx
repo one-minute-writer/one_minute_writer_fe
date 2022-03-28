6e4ae6359e90d3270738c672ce5a73b01b0cd7ba
Gabby Recny <gjtrecny@gmail.com>
2022-03-28 15:36:46 -0600

    Create fetch single user query and client

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,15 +3,31 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from '../Components/Dashboard/Dashboard';
 import WritingPage from '../Components/WritingPage/WritingPage';
+import { ApolloClient, InMemoryCache, ApolloProvider, useLazyQuery } from '@apollo/client';
+import { GET_SINGLE_USER } from '../Queries';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: `Insert fetch URL here`
+})
 
 const App: React.FC = () => {
+
+  const [ fetchUser, { loading, error, data } ] = useLazyQuery(GET_SINGLE_USER, {
+    variables: {
+      id: 1,
+    }
+  })
+
   return (
     <main>
-      <Routes>
-        <Route path='/' element={<Dashboard/>}/>
-        <Route path='/writing-page' element={<WritingPage/>}/>
-        {/* <Route path="*" element={<Errors/>}/> */}
-      </Routes>
+      <ApolloProvider client={ client }>
+        <Routes>
+          <Route path='/' element={<Dashboard/>}/>
+          <Route path='/writing-page' element={<WritingPage/>}/>
+          {/* <Route path="*" element={<Errors/>}/> */}
+        </Routes>
+      </ApolloProvider>
     </main>
   )
 }
