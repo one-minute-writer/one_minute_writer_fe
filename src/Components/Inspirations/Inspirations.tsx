f966db3409f14002232d85c24bc56d25373a94c2
Jacob Quintana <jacobquintaa@gmail.com>
2022-03-29 01:34:54 -0600

    Add styiling to inporation page '

--- src/Components/Inspirations/Inspirations.tsx
+++ src/Components/Inspirations/Inspirations.tsx
@@ -5,15 +5,20 @@ const Inspirations: React.FC = () => {
   return (
     <>
         <article className='inspirations'>
-          <div className='image-inspo' >
-            <img src="" alt="none" />
+          <div className='image-box' >
+            <img className='img-inspo' src="https://images.unsplash.com/photo-1555412654-72a95a495858?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" alt="none" />
           </div>
-          <div className='word-inspo' >
-            <h1>blossum</h1>
+          <button className='new-img'>New Image</button>
+          <div className='word-box' >
+            <h1 className='word-inspo'>blossum</h1>
           </div>
-          <div className='audio-inspo' >
-            <button type="button">audio message</button>
+          <button className='new-word'>New Word</button>
+          <div className="audio-box">
+            <div className="play-button">
+              <button className="play-arrow">Play</button>
+            </div>
           </div>
+          <button className='new-word'>New audio</button>
         </article>
     </>
   )
