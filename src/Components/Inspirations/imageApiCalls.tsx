a2257457010f95cca7059ecc058d76bb93991257
Lynn Nguyen <AmandaN22@outlook.com>
2022-03-29 21:18:30 -0600

    Edit error response

--- src/Components/Inspirations/imageApiCalls.tsx
+++ src/Components/Inspirations/imageApiCalls.tsx
@@ -3,9 +3,9 @@ import './Inspirations.scss'
 
 const checkForError = (response: Response) => {
   if (response.status === 404) {
-    throw (`${response.status} Error. Sorry, the page you're looking for doesn't exist.`)
+    throw (`${response.status} Error. Sorry, the image you're looking for doesn't exist.`)
   } else if ((!response.ok && response.status >= 400) && response.status < 500) {
-    throw (`${response.status} Error. Sorry, the page you're looking for doesn't exist.`)
+    throw (`${response.status} Error. Sorry, the image you're looking for doesn't exist.`)
   } else if (!response.ok && response.status >= 500) {
     throw (`${response.status} Error. Something went wrong. Please try again!`)
   } else if (!response.ok){
