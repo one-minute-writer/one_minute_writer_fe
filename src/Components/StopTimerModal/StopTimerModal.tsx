918bbef93f6bbc3d0362d40379a015bbc835a36d
Phil Lewis <phillewis1985@gmail.com>
2022-03-30 15:31:31 -0600

    Working on small changes

--- src/Components/StopTimerModal/StopTimerModal.tsx
+++ src/Components/StopTimerModal/StopTimerModal.tsx
@@ -8,7 +8,13 @@ interface Props {
   setShowModal: (arg0: boolean) => void
 }
 
-const StopTimerModal: React.FC<Props> = ({ toggleTimer, saveWriting, modalMessage, setShowModal }) => {
+const StopTimerModal: React.FC<Props> = ({
+    toggleTimer,
+    saveWriting,
+    modalMessage,
+    setShowModal
+  }) => {
+
   const continueWriting = () => {
     toggleTimer()
     setShowModal(false)
@@ -22,7 +28,10 @@ const StopTimerModal: React.FC<Props> = ({ toggleTimer, saveWriting, modalMessag
     <section className="modal">
       <div className="pop-up-container">
         <p className="end-timer-message">{modalMessage}</p>
-        <button className="save-writing-button" onClick={saveCurrentWriting}>Save</button>
+        <button
+          className="save-writing-button"
+          onClick={saveCurrentWriting}>Save
+        </button>
         <button className="continue-writing-button" onClick={continueWriting}>Continue Writing</button>
       </div>
     </section>
