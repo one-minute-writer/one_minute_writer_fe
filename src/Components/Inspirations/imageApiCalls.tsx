d96ade43f03b4ce8aab637352aced9e5d4e49074
Lynn Nguyen <AmandaN22@outlook.com>
2022-03-30 14:30:12 -0600

    Condense error handling

--- src/Components/Inspirations/imageApiCalls.tsx
+++ src/Components/Inspirations/imageApiCalls.tsx
@@ -2,9 +2,7 @@ import './Inspirations.scss'
 
 
 const checkForError = (response: Response) => {
-  if (response.status === 404) {
-    throw (`${response.status} Error. Sorry, the image you're looking for doesn't exist.`)
-  } else if ((!response.ok && response.status >= 400) && response.status < 500) {
+    if ((!response.ok && response.status >= 400) && response.status < 500) {
     throw (`${response.status} Error. Sorry, the image you're looking for doesn't exist.`)
   } else if (!response.ok && response.status >= 500) {
     throw (`${response.status} Error. Something went wrong. Please try again!`)
