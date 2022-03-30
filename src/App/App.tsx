d7921db23189c11ace014f15d8f14d5007deca62
Jacob Quintana <jacobquintaa@gmail.com>
2022-03-30 15:03:37 -0600

    Fix bugs

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,37 +3,20 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from '../Components/Dashboard/Dashboard';
 import WritingPage from '../Components/WritingPage/WritingPage';
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
-import { GET_SINGLE_USER} from '../Queries';
-
-
+import { useQuery, gql} from "@apollo/client"
+import { GET_SINGLE_USER } from '../Queries';
 
 const App: React.FC = () => {
 
-  const  { loading, error, data }  = useQuery(GET_SINGLE_USER, {
-    variables: {
-      id: 1,
-    }
-  })
-
-  // const [createUser, { loading: boolean, error, data }] = useLazyQuery(
-  //   CREATE_SINGLE_USER, {
-  //   variables: {
-  //     username: `Gigi Banks`,
-  //     email: `hello@hotmail.com`,
-  //   }
-  // }
-  // )
-
+  const get = useQuery(GET_SINGLE_USER)
+  
   return (
     <main>
-      {/* <ApolloProvider client={ client }> */}
         <Routes>
           <Route path='/' element={<Dashboard/>}/>
           <Route path='/writing-page' element={<WritingPage/>}/>
           {/* <Route path="*" element={<Errors/>}/> */}
         </Routes>
-      {/* </ApolloProvider> */}
     </main>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import './index.scss';
 import App from './App/App';
-import { ApolloClient, InMemoryCache, ApolloProvider, useLazyQuery } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
@@ -13,11 +13,13 @@ const client = new ApolloClient({
 console.log(client);
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ApolloProvider>,
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
+  </React.StrictMode >,
   document.getElementById('root')
 );
 
