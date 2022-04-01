52c3a24168142089a6e7f20a6c0de6d74b521f2c
Phil Lewis <phillewis1985@gmail.com>
2022-04-01 08:11:12 -0600

    Use mixins for styling modals

--- src/Components/ChooseTime/ChooseTime.tsx
+++ src/Components/ChooseTime/ChooseTime.tsx
@@ -9,8 +9,16 @@ const ChooseTime: React.FC<Props> = ({ setTimer }) => {
   
   
   return (
-    <section>
-      <input type="number"/>  
+    <section className="modal">
+      <div className="pop-up-container">
+        <select>
+          <option value={1}>1:00</option>
+          <option value={5}>5:00</option>
+          <option value={10}>10:00</option>
+          <option value={20}>20:00</option>
+        </select>
+      </div>
+      {/* <input type="number"/> */}
     </section>
   )
 }
--- src/Components/WritingPage/WritingPage.tsx
+++ src/Components/WritingPage/WritingPage.tsx
@@ -37,7 +37,7 @@ const WritingPage: React.FC = () => {
         }
         {showSetTimeModal && <ChooseTime setTimer={setTimer}/>}
       </section>
-        <TextInput />
+      <TextInput />
     </div>
   )
 }
