f796568815f15b5225ac8cb95568fa846080c1de
Phil Lewis <phillewis1985@gmail.com>
2022-04-02 17:05:46 -0600

    Remove comments

--- src/Components/WritingPage/WritingPage.tsx
+++ src/Components/WritingPage/WritingPage.tsx
@@ -1,44 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './WritingPage.scss';
 import Inspirations from '../Inspirations/Inspirations';
 import TextInput from '../TextInput/TextInput';
-import Timer from '../Timer/Timer'
-import ChooseTime from '../ChooseTime/ChooseTime'
 
 const WritingPage: React.FC = () => {
-  // const [ totalSeconds, setTotalSeconds ] = useState<number>(0)
-  // const [ timeChosen, setTimeChosen ] = useState(false)
-  // const [ showSetTimeModal, setShowSetTimeModal ] = useState(false)
-
-  // const setTimer = (seconds: number) => {
-  //   setTotalSeconds(seconds)
-  //   setTimeChosen(true)
-  // }
-
-  // const chooseTime = () => {
-  //   setShowSetTimeModal(true)
-  // }
-
-  // const saveWriting = () => {
-  //   //make the post request here
-  // }
-
   return (
     <div className="writing-page">
-      {/* <section className='inspirations-timer-box'> */}
         <Inspirations />
-        {/* {timeChosen ?
-          <Timer
-            saveWriting={saveWriting}
-            totalSeconds={totalSeconds}
-          /> :
-          <button onClick={chooseTime}>Choose Time</button>
-        }
-        {showSetTimeModal && <ChooseTime
-          setTimer={setTimer}
-          setShowSetTimeModal={setShowSetTimeModal}
-        />} */}
-      {/* </section> */}
       <TextInput />
     </div>
   )
