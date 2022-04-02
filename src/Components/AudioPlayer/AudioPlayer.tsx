22034d865a345bae06d6c6c4e9214c5a1b9615c3
Jacob Quintana <jacobquintaa@gmail.com>
2022-04-02 17:22:56 -0600

    install howler again

---
+++ src/Components/AudioPlayer/AudioPlayer.tsx
@@ -0,0 +1,30 @@
+import './AudioPlayer.scss';
+import { Howl, Howler } from 'howler';
+// import rain from '../audioClips'
+
+
+const AudioPlayer: React.FC = () => {
+
+ const callMySound = () => {
+  var sound = new Howl({
+   src: ['/audioClips/Rain.mp3'],
+   html5: true,
+  });
+  console.log(sound);
+
+  return sound.play();
+ }
+
+ return (
+  <>
+   <div onClick={() => callMySound()} className="audio-box">
+    <div className="play-button">
+     <button className="play-arrow">Play</button>
+    </div>
+   </div>
+   <button className='new-word'>New audio</button>
+  </>
+ )
+}
+
+export default AudioPlayer;
