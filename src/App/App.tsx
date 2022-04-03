0edf50f0047cd91c70ebee89ddd887bc570aeb56
Gabby Recny <gjtrecny@gmail.com>
2022-04-03 10:48:24 -0500

    Add error page

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from '../Components/Dashboard/Dashboard';
 import WritingPage from '../Components/WritingPage/WritingPage';
 import NavBar from '../Components/NavBar/NavBar'
+import Error from '../Components/Error/Error'
 
 const App: React.FC = () => {
   
@@ -13,7 +14,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path='/' element={<Dashboard/>}/>
         <Route path='/writing-page' element={<WritingPage/>}/>
-        {/* <Route path="*" element={<Errors/>}/> */}
+        <Route path="*" element={<Error/>}/>
       </Routes>
     </main>
   )
