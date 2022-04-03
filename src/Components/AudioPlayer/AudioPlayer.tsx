eef1f1848d91470ecc27a649eb76916ca1cd34c2
Jacob Quintana <jacobquintaa@gmail.com>
2022-04-02 22:48:54 -0600

    add audio files, create audio data

--- src/Components/AudioPlayer/AudioPlayer.tsx
+++ src/Components/AudioPlayer/AudioPlayer.tsx
@@ -4,19 +4,31 @@ import { Howl} from 'howler';
 
 const AudioPlayer: React.FC = () => {
 
- const audioOptions = []
+ const audioOptions = [
+  { title: 'At The Zoo', src: '/audioClips/at-zoo-audio.mp3'},
+  { title: 'Rain', src: '/audioClips/rain.mp3' },
+  { title: 'Fredrick Rosusseasu', src: '/audioClips/fredric.mp3' },
+  { title: 'Mjoras Mask', src: '/audioClips/majoras-mask.mp3' },
+  { title: 'Miles Davis Solo', src: '/audioClips/miles-davis.mp3' },
+  { title: 'Nature Audio', src: '/audioClips/nature-audio.mp3' },
+ ]
 
- const callMySound = () => {
+ const callMySound = (link: string) => {
   var sound = new Howl({
-   src: ['/audioClips/Rain.mp3'],
+   src: [link],
    html5: true,
   });
   return sound.play();
  }
 
+ function getRandomIndex() {
+  let randomSong = audioOptions[Math.floor(Math.random() * audioOptions.length)];
+  return callMySound(randomSong.src) 
+ }
+
  return (
   <>
-   <div onClick={() => callMySound()} className="audio-box">
+   <div onClick={() => getRandomIndex()} className="audio-box">
     <div className="play-button">
      <button className="play-arrow">Play</button>
     </div>
