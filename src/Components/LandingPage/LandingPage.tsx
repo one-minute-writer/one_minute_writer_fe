d8b90e99e24c5997c285e2a48df92dca42abfcf9
Phil Lewis <phillewis1985@gmail.com>
2022-04-05 11:58:46 -0600

    Style landing page header with className

--- src/Components/LandingPage/LandingPage.tsx
+++ src/Components/LandingPage/LandingPage.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const LandingPage: React.FC = () => {
   return (
     <section className="landing-page">
-      <h1>One Minute Writer</h1>
+      <h1 className="landing-page-header">One Minute Writer</h1>
       <p className="description">One minute writer is for creatives who are seeking inspiration and a space to document and organize their ideas and writing. Users can track their progress through metrics on a user dashboard as well as revisit and edit past writings.</p>
       <Link to='/dashboard'>
         <button className="go-to-dashboard-button">Go to dashboard</button>
