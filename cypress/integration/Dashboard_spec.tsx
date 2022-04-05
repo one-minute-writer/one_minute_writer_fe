2a3dd1f1be9bda2f9c17bb7a001c39f6bbe19f7f
Lynn Nguyen <AmandaN22@outlook.com>
2022-04-05 15:55:06 -0600

    Add classnames to test

--- cypress/integration/Dashboard_spec.tsx
+++ cypress/integration/Dashboard_spec.tsx
@@ -12,9 +12,8 @@ describe("User Dashboard", () => {
 })
 
   it('Should display the title, header, logo and nav bar', () => {
-    cy.get('img')
-      .should('be.visible')
-      .should('have.attr', 'alt')
+    cy.get('.logo').should('be.visible')
+      .get('[alt="One Minute Writer"]').should('exist')
       .get('header').should('be.visible')
       .get('nav').should('be.visible')
       .get('#dashboard-nav-button').should('exist')
@@ -22,9 +21,17 @@ describe("User Dashboard", () => {
       .get('#start-writing-nav-button').should('exist')
   })
 
-  it('should be able to get username on dashboard', () => {
+  it('should be able display data on dashboard', () => {
     cy.get('.welcome-user').should('have.text', 'Welcome, Pythagoras')
     .get('.words-per-min-display').should('have.text', '75')
     .get('.total-words-display').should('have.text', '1000')
+  })
+
+  it('should be able display users stories', () => {
+    cy.get('.single-story').should('exist')
+      .get('.title-display').first().should('have.text', 'Those Barren Leaves, Thrones, Dominations')
+      .get('.date-display').first().should('have.text', 'created: 04/03/2022')
+      .get('.bodyText-display').first().should('have.text', 'official')
+      .get('.edit-btn').first().should('exist')
   })
 })
