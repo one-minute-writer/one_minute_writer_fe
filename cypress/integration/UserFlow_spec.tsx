40f7da55f8325bd6089bd2b1dfa8c0e6adce43fb
Lynn Nguyen <AmandaN22@outlook.com>
2022-04-04 19:23:27 -0600

    Add user flow testing in cypress

--- cypress/integration/UserFlow_spec.tsx
+++ cypress/integration/UserFlow_spec.tsx
@@ -1,41 +1,83 @@
 describe('UserFlow', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4000/writing-page')
+  })
+
   it('should be able to get new inspiration image', () => {
-    cy.intercept('GET', 'https://one-minute-writer-be.herokuapp.com/graphql', { 
-      fixture: 'UserData.json' 
-    })
-    // .intercept('GET', 'http://localhost:4000')
-      .visit('http://localhost:4000', {
-      
-      })
-      
-      cy.get('header').should('exist')
-      .get('.menu') 
-      .get('#start-writing-nav-button').click()
-      .get('.img-inspo').should('exist')
-      .get('.new-img-btn').click()
-      .get('.img-inspo')   
+    cy.intercept('GET', 'https://picsum.photos/v2/list', [{ 
+      author: 'Roberto Nickson',
+      download_url: 'https://picsum.photos/id/1011/5472/3648'
+    }])
+    .visit('http://localhost:4000/writing-page')
+    .get('header').should('exist')
+    .get('.menu') 
+    .get('#start-writing-nav-button').click()
+    .get('.img-inspo').should('exist').should('have.attr', 'src').should('include','https://picsum.photos/id/1011/5472/3648')  
+    .get('.new-img-btn').click()
+    .intercept('GET', 'https://picsum.photos/v2/list', [{ 
+      author: 'Roberto Nickson',
+      download_url: 'https://picsum.photos/id/1011/5472/3648'
+    }])
+    .get('.img-inspo').should('exist').should('have.attr', 'src').should('include','https://picsum.photos/id/1011/5472/3648')  
   })
-
+  
   it('should be able to get new inspiration word', () => {
-
+    // cy.visit('http://localhost:4000/writing-page')
+    cy.get('.word-inspo').should('exist')
+    .get('.new-word-btn').click()
+    .get('.word-inspo').should('exist')
   })
-  it('should be able to get new inspiration sound', () => {
 
+  it('should be able to get new inspiration sound', () => {
+    
   })
   it('should be able to choose a time to start writing', () => {
-
+    // cy.visit('http://localhost:4000/writing-page')
+    cy.get('.choose-time-btn').should('exist').click()
+    .get('select')
+    .select('60')
+    .get('p').should('have.text', 'target: 01:00')
+    .get('.start-stop-btn').click()
+    .get('.start-stop-btn').click()
+    .get('.modal').should('exist')
+    .get('.continue-writing-button').click()
+    .get('.modal').should('not.exist')
   })
+  it('should have a pop up a modal when time is up', () => {
+    // cy.visit('http://localhost:4000/writing-page')
+    cy.get('.choose-time-btn').should('exist').click()
+    .get('select')
+    .select('60')
+    .get('p').should('have.text', 'target: 01:00')
+    .get('.start-stop-btn').click()
+    .wait(60000)
+    .get('.modal').should('exist')
+    .get('.end-timer-message').should('have.text', 'Your time is up! Would you like to keep writing or save your work?')
+  })
+
   it('should be able to enter a title', () => {
-
+    // cy.visit('http://localhost:4000/writing-page')
+    cy.get('input').type('One minute writer')
+    .get('input').should('have.value', 'One minute writer')
   })
-  it('should be able to start writing in the text area', () => {
 
+  it('text area should be disabled if user has not chosen a time', () => {
+    // cy.visit('http://localhost:4000/writing-page')
+  })
+
+  it('should be able to start writing in the text area', () => {
+    
   })
   it('should be able to save their writing', () => {
-
+    
   })
   it('should be able to go to the dashboard and see their writing', () => {
-
+    
   })
 })
 
+
+// cy.intercept('GET', 'https://one-minute-writer-be.herokuapp.com/graphql', { 
+//   fixture: 'UserData.json' 
+// })
+// .visit('http://localhost:4000/')
