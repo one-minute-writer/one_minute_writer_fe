070781319a9a319f5246e51edbd7ad1611efca5e
Phil Lewis <phillewis1985@gmail.com>
2022-04-05 11:26:07 -0600

    Style landing page

--- src/Components/LandingPage/LandingPage.tsx
+++ src/Components/LandingPage/LandingPage.tsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 
 const LandingPage: React.FC = () => {
   return (
-    <section>
+    <section className="landing-page">
       <h1>One Minute Writer</h1>
-      <p>One minute writer is for creatives who are seeking inspiration and a space to document and organize their ideas and writing. Users can track their progress through metrics on a user dashboard as well as revisit and edit past writings.</p>
-      <Link to='/dashboard/'>
-        <button>Go to dashboard</button>
+      <p className="description">One minute writer is for creatives who are seeking inspiration and a space to document and organize their ideas and writing. Users can track their progress through metrics on a user dashboard as well as revisit and edit past writings.</p>
+      <Link to='/dashboard'>
+        <button className="go-to-dashboard-button">Go to dashboard</button>
       </Link>
     </section>
   )
