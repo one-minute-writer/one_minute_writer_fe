620d4d461b8ee3709209c9cbb347795c6bb9127b
Lynn Nguyen <AmandaN22@outlook.com>
2022-04-05 14:37:15 -0600

    Delete Header test and add to UserData test

--- cypress/integration/UserData_spec.tsx
+++ cypress/integration/UserData_spec.tsx
@@ -1,6 +1,6 @@
 import { aliasQuery } from "../utils/graphql-test-utils.js";
 
-describe("User Dashboard Queried Data", () => {
+describe("User Dashboard", () => {
   beforeEach(() => {
     cy.intercept(
       "POST",
@@ -8,12 +8,23 @@ describe("User Dashboard Queried Data", () => {
       (req) => {
         aliasQuery(req, "fetchUser", "fetchUser.json")
   })
+    cy.visit('http://localhost:4000') 
 })
 
+  it('Should display the title, header, logo and nav bar', () => {
+    cy.get('img')
+      .should('be.visible')
+      .should('have.attr', 'alt')
+      .get('header').should('be.visible')
+      .get('nav').should('be.visible')
+      .get('#dashboard-nav-button').should('exist')
+      .get('#dashboard-nav-button').should('have.css', 'color', 'rgb(161, 127, 26)')
+      .get('#start-writing-nav-button').should('exist')
+  })
+
   it('should be able to get username on dashboard', () => {
-    cy.visit("http://localhost:4000/")
-      .get('.username-display').should('have.text', 'Welcome, Pythagoras')
-      .get('.words-per-minute').should('have.text', '75')
-      .get('.total-words').should('have.text', '1000')
+    cy.get('.username-display').should('have.text', 'Welcome, Pythagoras')
+    .get('.words-per-minute').should('have.text', '75')
+    .get('.total-words').should('have.text', '1000')
   })
 })
