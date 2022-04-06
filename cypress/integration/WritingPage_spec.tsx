2ebe31231dc6c5f7e410e937c21e8d57b924dcbd
Lynn Nguyen <AmandaN22@outlook.com>
2022-04-06 11:21:17 -0600

    Pass all of writing page tests

--- cypress/integration/WritingPage_spec.tsx
+++ cypress/integration/WritingPage_spec.tsx
@@ -1,33 +1,33 @@
 describe('Writing Page', () => {
-  // beforeEach(()=> {
-  //   cy.intercept('GET', 'https://picsum.photos/v2/list', [{ 
-  //   author: 'Roberto Nickson',
-  //   download_url: 'https://picsum.photos/id/1011/5472/3648'
-  // }])
-  //     .visit('https://one-minute-writer.herokuapp.com/writing-page') 
-  // })
+  beforeEach(()=> {
+    cy.intercept('GET', 'https://picsum.photos/v2/list', [{ 
+    author: 'Roberto Nickson',
+    download_url: 'https://picsum.photos/id/1011/5472/3648'
+  }])
+      .visit('https://one-minute-writer.herokuapp.com/writing-page') 
+  })
 
-  // it('show the header', () => {
-  //   cy.get('header').should('be.visible')
-  // })
+  it('show the header', () => {
+    cy.get('header').should('be.visible')
+  })
 
-  // it('should have an inspiration section', () => {
-  //     cy.get('.inspirations').should('be.visible')
-  //       .get('.img-container').should('be.visible')
-  //       .get('img').should('be.visible')
-  //       .get('.new-img-btn').should('be.visible')
-  //       .get('.word-inspiration').should('be.visible')
-  //       .get('.word-inspo').should('be.visible')
-  //       .get('.new-word-btn').should('be.visible')
-  //       .get('.audio-inspiration').should('be.visible')
-  //       .get('.new-audio-btn').should('be.visible')
-  // })
+  it('should have an inspiration section', () => {
+      cy.get('.inspirations').should('be.visible')
+        .get('.img-container').should('be.visible')
+        .get('img').should('be.visible')
+        .get('.new-img-btn').should('be.visible')
+        .get('.word-inspiration').should('be.visible')
+        .get('.word-inspo').should('be.visible')
+        .get('.new-word-btn').should('be.visible')
+        .get('.audio-inspiration').should('be.visible')
+        .get('.new-audio-btn').should('be.visible')
+  })
 
-  // it('should have a writing section', () => {
-  //   cy.get('input').should('be.visible')
-  //     .should('have.attr', 'placeholder','Title...')
-  //     .get('textarea').should('be.visible')
-  //     .should('have.attr', 'placeholder', 'Begin writing here...')
-  //     .should('be.disabled')
-  // })     
+  it('should have a writing section', () => {
+    cy.get('input').should('be.visible')
+      .should('have.attr', 'placeholder','Title...')
+      .get('textarea').should('be.visible')
+      .should('have.attr', 'placeholder', 'Begin writing here...')
+      .should('be.disabled')
+  })     
 })
