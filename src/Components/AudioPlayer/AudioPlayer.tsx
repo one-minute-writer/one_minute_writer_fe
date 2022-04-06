b4fd29590f42a668890790e6c7d671652abaff8c
Gabby Recny <gjtrecny@gmail.com>
2022-04-05 23:02:15 -0500

    Seperate pause and play buttons

--- src/Components/AudioPlayer/AudioPlayer.tsx
+++ src/Components/AudioPlayer/AudioPlayer.tsx
@@ -82,10 +82,10 @@ const AudioPlayer: React.FC<Props> = ({ setSound }) => {
 
   return (
     <>
-      <article className='audio-box'>
+      <section className='audio-box'>
         <img onClick={(event) => { playMusic(event)}} className={`${playClick}`} src={playButton} alt="play-button" />
         <img onClick={(event) => { pauseMusic(event) }} className={`${pauseClick}`} src={pauseButton} alt="play-button" />
-      </article>
+      </section>
       <button onClick={() => generateNewSong()} className='new-audio-btn'>New Audio</button>
     </>
   )
